39a6bf610c159cef502ca490b7348557d20ed0ee
cloud-ray <ray@cybersavvy.one>
2024-08-24 09:29:09 -0400

    Updated script fetch

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import logging
 
 app = Flask(__name__)
 # CORS(app, resources={r"/process": {"origins": ["http://localhost:8080", "http://127.0.0.1:8080"]}})
-CORS(app, resources={r"/*": {"origins": "*"}})
-# CORS(app)
+# CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app)
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
